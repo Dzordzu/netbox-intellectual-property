bfb820a3948431888f282e916ac6994b823d108c
Tomasz Durda <tomekdur@wp.pl>
2020-07-22 14:02:25 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-services-types',
+    version='0.1',
+    description='Services types for netbox',
+    url='https://github.com/Dzordzu/netbox-services-types',
+    author='Tomasz Durda',
+    license='MIT',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+)

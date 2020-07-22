6e39a0634cd786d63ebca9d8fc152797eb2d631e
Tomasz Durda <tomekdur@wp.pl>
2020-07-22 20:35:01 +0200

    It works!

--- netbox_services_types/navigation.py
+++ netbox_services_types/navigation.py
@@ -1,13 +1,13 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
-# menu_items = (
-#     PluginMenuItem(
-#         link='plugins:netbox-services-types:service-type',
-#         link_text='Random service type',
-#         buttons=(
-#             PluginMenuButton('home', 'Button A', 'fa fa-info', ButtonColorChoices.BLUE),
-#             PluginMenuButton('home', 'Button B', 'fa fa-warning', ButtonColorChoices.GREEN),
-#         )
-#     ),
-# )
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_services_types:service_type',
+        link_text='Random service type',
+        buttons=(
+            PluginMenuButton('home', 'Button A', 'fa fa-info', ButtonColorChoices.BLUE),
+            PluginMenuButton('home', 'Button B', 'fa fa-warning', ButtonColorChoices.GREEN),
+        )
+    ),
+)
--- netbox_services_types/views.py
+++ netbox_services_types/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import View
 from .models import ServiceType
 
 class ServiceTypeView(View):
     def get(self, request):
         serviceType = ServiceType.objects.all()
+        # return  HttpResponse(':)', status=200)
         return render(request, 'netbox_services_types/service_type.html', {
             'serviceType': serviceType,
         })

a16f01a79750fc990417d943eb96fa51929e4a6e
Tomasz Durda <tomekdur@wp.pl>
2020-07-31 01:47:44 +0200

    Changed navigation

--- netbox_licences/navigation.py
+++ netbox_licences/navigation.py
@@ -6,14 +6,22 @@ menu_items = (
         permissions = ['netbox_licences.view_softwareprovider'],
         link='plugins:netbox_licences:software_providers_list',
         link_text='Software Providers',
-        buttons=(
-            PluginMenuButton('home', 'Button A', 'fa fa-info', ButtonColorChoices.BLUE),
-            PluginMenuButton('home', 'Button B', 'fa fa-warning', ButtonColorChoices.GREEN),
-        )
+        # buttons=(
+        #     PluginMenuButton('home', 'Button A', 'fa fa-info', ButtonColorChoices.BLUE),
+        #     PluginMenuButton('home', 'Button B', 'fa fa-warning', ButtonColorChoices.GREEN),
+        # )
     ),
     PluginMenuItem(
         permissions = ['netbox_licences.view_licence'],
         link='plugins:netbox_licences:licences_list',
         link_text='Licences',
+        buttons=(
+            PluginMenuButton(
+                'plugins:netbox_licences:software_providers_add',
+                'Add Software Provider',
+                'fa fa-plus',
+                ButtonColorChoices.GREEN
+            ),
+        )
     ),
 )

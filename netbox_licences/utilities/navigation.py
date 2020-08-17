441abe09e1e507218ad7cbf68ac54bf86aeb0cd5
Tomasz Durda <tomekdur@wp.pl>
2020-08-17 12:48:37 +0200

    Generalized package name

--- netbox_licences/utilities/navigation.py
+++ netbox_licences/utilities/navigation.py
@@ -1,15 +1,16 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
+from .. import config
 import inflection
 
 def plugin_item(name):
     return PluginMenuItem(
-        permissions = [f"netbox_licences.view_{name.lower()}"],
-        link=f"plugins:netbox_licences:{inflection.tableize(name)}_list",
+        permissions = [f"{config.name}.view_{name.lower()}"],
+        link=f"plugins:{config.name}:{inflection.tableize(name)}_list",
         link_text=f"{inflection.titleize(inflection.pluralize(name))}",
         buttons=(
             PluginMenuButton(
-                f"plugins:netbox_licences:{inflection.tableize(name)}_add",
+                f"plugins:{config.name}:{inflection.tableize(name)}_add",
                 f"Add {inflection.titleize(name)}",
                 'fa fa-plus',
                 ButtonColorChoices.GREEN
